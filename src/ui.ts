7578f93e623e067aa0765d23b5263d578eee3409
Sebastian <sebastiangutierrezcorrea@gmail.com>
2019-08-26 00:12:47 -0500

    Add message and tip to html

--- src/ui.ts
+++ src/ui.ts
@@ -1,9 +1,28 @@
 import './lib/figma-plugin-ds.min.css'
 import './lib/figma-plugin-ds.min.js'
+import './ui.css'
 
 selectMenu.init();
 
 document.getElementById('run').onclick = () => {
   const fakerLocation = document.getElementById('location').value
   parent.postMessage({ pluginMessage: { type: 'run' , fakerLocation} }, '*')
+}
+
+onmessage = (event) => {
+  if ( event.data.pluginMessage.type === 'error' ) {
+    showBell(event.data.pluginMessage.msg)
+  }
+}
+
+function showBell (msg) {
+  const bell = document.getElementById('visual-bell')
+  const messageBell = document.querySelector('.visual-bell__msg')
+
+  messageBell.textContent = msg
+  bell.classList.toggle('hidde')
+
+  setTimeout(function () {
+    bell.classList.toggle('hidde')
+  }, 5000)
 }
